3a96643943c290d19ece41abd2a2fd9a46badd6b
201504 48076 Tyler Race <tyler.race@syccuxas01.pcc.edu>
2016-04-06 22:35:52 -0700

    First commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/* 
+   The grocery store reciept
+   Written by: Tyler Race
+   Date: 04/01/16
+   Purpose: Keep a running total of a shopping list
+   Sources: None
+*/
+
+
+#include <iostream>
+#include <string>
+#include <string.h>  // for strcpy to copy char arrays
+
+using namespace std;
+
+int main ()
+{
+	// Variables
+	
+	// Code
+	"string example"	
+
+	return 0;	
+}
